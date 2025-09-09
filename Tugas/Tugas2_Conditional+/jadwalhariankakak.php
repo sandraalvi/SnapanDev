8f1dfa7eccfe7662e7b6ab206664f2c58fe1be5d
keilahacker2045 <adalilatisya@gmail.com>
2025-09-09 16:46:03 +0700

    qila & keisha X PPLG 2

---
+++ Tugas/Tugas2_Conditional+/jadwalhariankakak.php
@@ -0,0 +1,22 @@
+<?php
+
+//Keisha Putri Ratri X PPLG 2//
+
+$jam = 00.00 ;
+if ($jam >= 3 && $jam < 5) {echo "Kakak (beribadah) kemudian main hp 𐔌՞. .՞𐦯 🤳";}
+elseif ($jam && 5 && $jam < 5.30) {echo "Kakak sedang siap-siap 𓂃 mandi pagi (ᵕ—ᴗ—) 😶‍🌫️";}
+elseif ($jam >= 5.30 && $jam < 6) {echo "Kakak sarapan sebelum berangkat sekolah 𓂃 ⌞ 🥓 ⌝";}
+elseif ($jam && 6 && $jam < 6.30) {echo "Kakak sedang berangkat sekolah 𓂃 🚗 ᯓ";}
+elseif ($jam && 6.30 && $jam < 7) {echo "Kakak sampai disekolah ﾒ 🪂";}
+elseif ($jam >= 7 && $jam < 15.30) {echo "Kakak melaksanakan KBM ✎ᝰ. 📖";}
+elseif ($jam >= 15.45 && $jam < 17.00) {echo "Kakak mengikuti ekstrakulikuler voice 8 ꒰ 🎙️ ꒱";}
+elseif ($jam >= 17.00 && $jam < 17.10) {echo "Kakak mampir ke fotocopy -ˋˏ✄┈┈┈┈ 📃";}
+elseif ($jam >= 17.15 && $jam < 18.15) {echo "Kakak mengerjakan tugas 𓍯𓂃 📔";}
+elseif ($jam >= 18.15 && $jam < 18.45) {echo "Kakak makan bersama keluarga 𐂯 🍻";}
+elseif ($jam >= 18.45 && $jam < 19.05) {echo "Kakak berbincang bersama keluarga 𖣂 👧👦";}
+elseif ($jam >= 19.05 && $jam < 20.35) {echo "Kakak istirahat saat free time ಇ. 🎮";}
+elseif ($jam >= 20.35 && $jam < 21.05) {echo "Kakak membuka computer untuk coding 𖤝 🧑‍💻";}
+elseif ($jam >= 21.05 && $jam < 21.15) {echo "Kakak melakukan persiapan  untuk besok 𝒾 🎒";}
+elseif ($jam >= 21.15 && $jam < 21.45) {echo "Kakak membuka computer untuk coding ◡̈ 🧑‍💻";}
+else {echo "Kakak sedang tidur ᶻ 𝘇 𐰁 😴";}
+?>
---
+++ Tugas/Tugas2_Conditional+/kirishima.php
@@ -0,0 +1,22 @@
+<?php
+
+ // Aqila Dalilatisya X PPLG 2 //
+ 
+$jam = 12.54 ;
+if ($jam >= 3 && $jam < 5) {echo "Kakak (beribadah) kemudian main hp.";}
+elseif ($jam && 5 && $jam < 5.30) {echo "Kakak sedang siap-siap 𓂃 mandi pagi.";}
+elseif ($jam >= 5.30 && $jam < 6) {echo "Kakak sarapan sebelum berangkat sekolah.";}
+elseif ($jam && 6 && $jam < 6.30) {echo "Kakak sedang berangkat sekolah.";}
+elseif ($jam && 6.30 && $jam < 7) {echo "Kakak sampai disekolah.";}
+elseif ($jam >= 7 && $jam < 15.30) {echo "Kakak melaksanakan KBM.";}
+elseif ($jam >= 15.45 && $jam < 17.00) {echo "Kakak mengikuti ekstrakulikuler voice.";}
+elseif ($jam >= 17.00 && $jam < 17.10) {echo "Kakak mampir ke fotocopy.";}
+elseif ($jam >= 17.15 && $jam < 18.15) {echo "Kakak mengerjakan tugas.";}
+elseif ($jam >= 18.15 && $jam < 18.45) {echo "Kakak makan bersama keluarga.";}
+elseif ($jam >= 18.45 && $jam < 19.05) {echo "Kakak berbincang bersama keluarga.";}
+elseif ($jam >= 19.05 && $jam < 20.35) {echo "Kakak istirahat saat free time.";}
+elseif ($jam >= 20.35 && $jam < 21.05) {echo "Kakak membuka computer untuk coding.";}
+elseif ($jam >= 21.05 && $jam < 21.15) {echo "Kakak melakukan persiapan  untuk besok.";}
+elseif ($jam >= 21.15 && $jam < 21.45) {echo "Kakak membuka computer untuk coding.";}
+else {echo "Kakak sedang tidur.";}
+?>
