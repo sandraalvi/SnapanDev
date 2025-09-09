cf511deb95a909d972b2003f7cc26a46c8c92cbb
hekkerjubahmerahranisiu <noret0811@gmail.com>
2025-09-09 16:27:11 +0700

    Add files via upload
    MaharaniRosyadahN.X-PPLG2

---
+++ Tugas/Tugas1_Conditional/hari1.php
@@ -0,0 +1,16 @@
+<?php
+$NILAI = 0;
+
+ if ($NILAI > 100) {echo "Nilai tidak valid :p";}
+
+ elseif ($NILAI >= 90) {echo "$NILAI = Nilai A";}
+
+elseif ($NILAI >= 80) {echo "$NILAI = Nilai B";}
+
+elseif ($NILAI >= 70) {echo "$NILAI = Nilai C";}
+
+elseif ($NILAI >= 60) {echo "$NILAI = Nilai D";}
+
+else {echo "$NILAI = Nilai E";}
+
+?>
