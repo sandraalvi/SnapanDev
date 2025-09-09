52bc420241c75c0507663bbc5681b3fdada2de21
fanesa114 <fanezzaananditaputri@gmail.com>
2025-09-09 16:30:56 +0700

    Add files via upload

---
+++ Tugas/Tugas1_Conditional/belajarphp1.php
@@ -0,0 +1,41 @@
+<?php
+//nilai A=100-90
+//NILAI B=89-80
+//Nilai C=79-70
+//Nilai D=69-60
+//Nilai diatas 100 dianggap tidak valid
+
+$nilai = 99;
+//jika,variabel nilai memiliki kurang dari atau sama dengan 100
+//dan,variabel nilai memiliki lebih dari atau sama dengan 90
+if ($nilai <= 100 && $nilai >= 90) {
+echo "nilai A";
+}
+$nilai = 88;
+//jika,variabel memiliki kurang dari atau sama dengan 89
+//dan,variabel memiliki lebih dari atau sama dengan 80
+
+if ($nilai <= 89 && $nilai >= 80) {
+echo "Nilai B";
+}
+$nilai = 78;
+//jika ,variabel memiliki kurang dari atau sama dengan 79
+//dan,variabel memiliki lebih dari atau sama dengan 70
+if ($nilai <= 79 && $nilai >= 70) {
+echo "Nilai C";
+}
+$nilai = 68;
+//jika,variabel memiliki kurang dari atau dengan 69
+//dan,variabel memiliki lebih dari ata sama dengan 60
+if($nilai <= 69 && $nilai >= 60) {
+echo "Nilai D";
+}
+$nilai = 95;
+//jika,variabel nilai memiliki kurang dari atau sama dengan 59
+//den,variabel nilai memiliki lebih dari atau sama dengan 100
+if($nilai <= 59 && $nilai >= 100 ) {
+echo "Nilai luar biasa" ;
+}
+
+
+
