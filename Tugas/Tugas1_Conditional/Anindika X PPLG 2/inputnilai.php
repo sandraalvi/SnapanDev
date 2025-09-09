b674ea50718cdcf7d987a60d8897b8868af64d40
anindnyls <aninsuja@gmail.com>
2025-09-09 16:23:06 +0700

    Add files via upload

---
+++ Tugas/Tugas1_Conditional/Anindika X PPLG 2/inputnilai.php
@@ -0,0 +1,9 @@
+<?php
+$NILAI = 20;
+if ($NILAI > 100) {echo "nilai tidak valid :p";}
+elseif ($NILAI >= 95) {echo "$NILAI = nilai A";}
+elseif ($NILAI >= 85) {echo "$NILAI = nilai B";}
+elseif ($NILAI >= 75) {echo "$NILAI = nilai C";}
+elseif ($NILAI >= 65) {echo "$NILAI = nilai D";}
+else {echo "$NILAI = nilai E";}
+?>
