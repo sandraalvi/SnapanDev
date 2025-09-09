f36c19c60cfc4bac37574fefbd90d48db71781ca
keilahacker2045 <adalilatisya@gmail.com>
2025-09-09 16:24:48 +0700

    Add files via upload

---
+++ Tugas/Tugas1_Conditional/inputnilaitugas.php
@@ -0,0 +1,12 @@
+<?php
+
+ //Keisha Putri Ratri X PPLG 2//
+
+$NILAI = 79;
+if ($NILAI > 100) {echo "Nilai tidak valid :p";}
+ elseif ($NILAI >= 90) {echo "$NILAI = Nilai A";}
+elseif ($NILAI >= 80) {echo "$NILAI = Nilai B";}
+elseif ($NILAI >= 70) {echo "$NILAI = Nilai C";}
+elseif ($NILAI >= 60) {echo "$NILAI = Nilai D";}
+else {echo "$NILAI = Nilai E";}
+?>
